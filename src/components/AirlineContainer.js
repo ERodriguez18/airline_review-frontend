67ec1c5d07cfdc326dfedd87d857446d020aabd6
Erick <erisrnb18@gmail.com>
2021-08-12 20:09:48 -0400

    export fix

--- src/components/AirlineContainer.js
+++ src/components/AirlineContainer.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import BASE_URL from '../constraints/index.js';
+import {BASE_URL} from '../constraints/index.js';
 
 
 export default function AirlineContainer() {
